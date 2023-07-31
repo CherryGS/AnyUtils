d63385e558b6866f7b128387d867dfc50b439da2
CherryGS <tickten@outlook.com>
2023-07-31 10:02:20 +0800

    upd

--- anyutils/file.py
+++ anyutils/file.py
@@ -5,7 +5,9 @@ from ._logger import rootLogger as logger
 
 
 def scan_folder(path: Path, dfs: bool = True):
-    """快速扫描指定 `path` 下文件"""
+    """快速扫描指定 `path` 下文件, 如果 `path` 是文件则返回自身."""
+    if path.is_file():
+        return [str(path.resolve(strict=True))]
     folder = [str(path.resolve(strict=True))]
     file: list[str] = list()
     while folder:
